7fc782fc1f5aba7715c046776e98da0f2c8947dc
am <anthony.martin2019@campus-eni.fr>
2020-04-28 16:10:38 +0200

    Page layout.html.twig à jour avec l'utilisation de Encore et après tests (importation du fichier app.css).
    -> Nécessite l'installation préalable de Bootstrap et Bootsrap JavaScript pour fonctionner + configuration dans les fichiers.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
+require('bootstrap');
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,7 +41,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="json", nullable=true)
      */
     private $roles = [];
 
