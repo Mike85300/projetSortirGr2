730c8d2d6458e2250426b135a0f234287ef2368d
am <anthony.martin2019@campus-eni.fr>
2020-04-30 00:23:14 +0200

    UserController.php à jour pour l'affichage et la modification du profil.
    Page profile.html.twig

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Entity\User;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -18,8 +21,8 @@ class UserController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('trip_dashboard');
-         }
+            return $this->redirectToRoute('trip_dashboard');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -37,39 +40,47 @@ class UserController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+
     /**
-     * @Route("/user/profile"), name="user_profile")
+     * @author : Anthony Martin
+     * @Route("/user/profile", name="user_profile")
      */
     public function profile(Request $request)
     {
         $user = $request->getUser();
         return $this->render('user/profile.html.twig', [
-            "user"=>$user
+            "user" => $user
         ]);
     }
+
 
     /**
      * @Route("user/edit", name="user_edit")
      */
     public function edit(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
     {
-        $user = $request->getUser();
+        $user = $this->getUser(User::class);
         $userForm = $this->createForm(UserType::class, $user);
+
         $userForm->handleRequest($request);
 
-        if($userForm->isSubmitted() && $userForm->isValid()){
-            $password = get('password')->getData;
-            if (!empty($password)){
-                $hashed=$encoder->encodePassword($user, $password);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $password = $userForm->get("password")->getData();
+            if (!empty($password)) {
+                $hashed = $encoder->encodePassword($user, $password);
                 $user->setPassword($hashed);
-                $em->persist($user);
-                $em->flush();
             }
+            $this->addFlash('success', 'Profil mis à jour !');
+            $em->persist($user);
+            $em->flush();
 
+            return $this->redirectToRoute('trip_dashboard');
         }
 
         return $this->render('user/edit.html.twig', [
-            "user"=>$user, "userForm"=>$userForm->createView()
+            "user" => $user, "userForm" => $userForm->createView()
         ]);
     }
+
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,8 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('dashboard', './assets/js/dashboard.js')
     .addEntry('login', './assets/js/login.js')
+    .addEntry('profile', './assets/js/profile.js')
+
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
