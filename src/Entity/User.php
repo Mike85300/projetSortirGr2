bb7662db79e817ad1022b5c06bb147d1005da991
romaintanguy <rtanguy@live.fr>
2020-04-29 18:45:37 +0200

    Update User.php : stockage du rôle en base de données + ROLE_ADMIN

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -118,8 +118,6 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
@@ -217,8 +215,6 @@ class User implements UserInterface
 
         return $this;
     }
-
-
 
 
     /**
