bc137eceace2133c005f27180ff4e5cb6a032c2d
am <anthony.martin2019@campus-eni.fr>
2020-04-30 10:36:25 +0200

    Page layout.html.twig à jour avec l'utilisation de Encore et après tests (importation du fichier app.css).
    -> Nécessite l'installation préalable de Bootstrap et Bootsrap JavaScript pour fonctionner + configuration dans les fichiers.

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,11 +7,9 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -24,13 +22,13 @@ class UserType extends AbstractType
             ->add('firstname')
             ->add('phone')
             ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Mot de passe différent. Veuillez réessayer.',
-                'mapped' => false,
-                'first_options' => ['label' => 'Veuillez saisir votre mot de passe'],
-                'second_options' => ['label' => 'Veuillez saisir à nouveau votre mot de passe'],
-            ])
+            ->add('password', RepeatedType::class,
+                array('mapped' => false,
+                    'required' => false,
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Mot de passe différent. Veuillez réessayer.',
+                    'first_options' => ['label' => 'Nouveau mot de passe'],
+                    'second_options' => ['label' => 'Confirmer mot de passe']))
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'name']);
