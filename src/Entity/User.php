164281c159497c2adb2de449064f5aabaf2ce678
am <anthony.martin2019@campus-eni.fr>
2020-04-30 17:46:31 +0200

    MAJ de l'entité USER : unicité du mail et du pseudo. Et le composer.lock... Personne ne sait vraiment pourquoi...

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,13 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * @UniqueEntity(fields={"username"}, message="Pseudo déjà utilisé.")
+ * @UniqueEntity(fields={"email"}, message="E-mail déjà utilisé.")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table(name="users")
  */
